a89b5524222f77c308bb2382ec619c5c2a88d888
rafasilverio <rafasilverio0@gmail.com>
2013-10-09 22:45:54 -0300

    Update quickfind.c

--- quickfind.c
+++ quickfind.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*Algoritmo que faz uma busca no vetor simulando as funções do QuickSort*/
+
+/*Função que particiona o vetor em pedaços, recebe o vetor, e as suas pontas(inicio e fim)*/
 int Particione (int A[], int p, int r) {
    int x = A[r];
    int i = p - 1;
@@ -17,31 +20,34 @@ int Particione (int A[], int p, int r) {
    return i + 1;
 }
 
+/*função que identifica o valor passado no vetor*/
 void QuickFind (int A[], int p, int r, int k) {
   if (p <= r) {
-     int q = Particione (A, p, r);
-     if (q == k) {
+     int q = Particione (A, p, r); /*divide o vetor em 2 e retorna uma "chave"*/
+     if (q == k) { /*verifica se o valor de retorno é igual passado. Achou!*/
         printf("Elemento: %d\n", A[q]);
         return; 
      }
-     else if (q < k)
+     else if (q < k) /*Se for menor percorre a direita do vetor*/
             QuickFind (A, q+1, r, k); 
-     	  else
+     	  else /*Se for maior percorre a esquerda do vetor*/
             QuickFind (A, p, q-1, k);
   }
 }
 
 
 int main (){
+   /*variáveis*/
     int i, p = 0, r = 7;
-
     int A[] = {7, 1, 3, 10, 17, 2, 21, 9};
  
+    /*Mostra os elementos do vetor*/
     printf("Elementos de entrada: \n");
     for (i = 0; i <= r; i++) {
        printf("%d ", A[i]);
     }
 
+    /*chamada da função*/
     QuickFind (A, p, r, 7);
     
     printf("\n");
